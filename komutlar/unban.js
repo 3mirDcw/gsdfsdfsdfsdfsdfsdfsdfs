aba564a14a859aa8a2215f761cd7c243db918e9c
Glitch (show-team-3mir) <none>
2021-01-24 13:44:37 +0000

    🎄📍 Checkpoint ./komutlar/çekilişbitir.js:27438194/4020 ./komutlar/goldekle.js:27438194/1191 ./komutlar/goldkaldır.js:27438194/2480 ./komutlar/ban.js:27438194/1228 ./komutlar/unban.js:27438194/2360

--- komutlar/unban.js
+++ komutlar/unban.js
@@ -1,51 +1,31 @@
-const codare = require('discord.js')
-exports.run = async(message, args) => {
+//---Bu Kod komutlar klasörüne atılacaktır.
+//###CodeMareFi tarafından hazırlanmıştır - - - Ekleyen //###Sadista
 
-if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`<a:by:752306236606906399>  **Bu komutu kullanabilmek için** "\`Yönetici\`" **yetkisine sahip olmalısın.**`);
+const Discord = require("discord.js");
+exports.run = async function(client, message, args) {
 
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(`**\`Yönetici\`** İznine Sahip Olman Gerek.`)
+  let yasak = await message.guild.fetchBans();
+  yasak = yasak.array(); 
+  yasak.forEach(function(ban) {
+    message.guild.unban(ban.id);
 
-let tanersins = args[0]
-if(!tanersins) {
-  message.channel.send(new codare.MessageEmbed())
-.setTitle("Hatalı!")
-.setDescription(`Lütfen yasağı kaldırılacak kişinin kullanıcı ID’sini giriniz`)
-.setFooter(`${message.author.tag} Tarafından istendi!`)
-.setColor("RED")
-if (isNaN(tanersins)) {
-  message.channel.send(new codare.MessageEmbed())
-.setTitle("Hatalı!")
-.setDescription(`Lütfen doğru bir ID Giriniz!`)
-.setColor("RED")
-.setFooter(`${message.author.tag} Tarafından istendi!`)
+        message.channel.send(`${yasak} Adlı Tüm Kullanıcıların Yasağı Kalktı!`);
 
-const embeed = await message.guild.fetchBans();
-message.guild.members.unban(tanersins)  
-if (!tanersins.id === embeed) {
-  message.channel.send(new codare.MessageEmbed())
-.setTitle("Hatalı!")
-.setDescription(`Bu kişi zaten yasaklanmış!`)
-.setColor("RED")
-.setFooter(`${message.author.tag} Tarafından istendi!`)
+  });
+            
+   
+};
 
-  message.channel.send(new codare.MessageEmbed())
-.setTitle("Başarılı!")
-.setDescription(`<@!${tanersins}> Adlı kullanıcının yasağı başarı bir şekilde kaldırıldı!`)
-.setColor("GREEN")
-.setFooter(`${message.author.tag} Tarafından istendi!`)
-}
-}
-}
-
-}
-//codare
 exports.conf = {
   enabled: true,
-  guildOnly: false,
-  aliases: [],
+  guildOnly: true,
+  aliases: ['unban'],
   permLevel: 0
-}
+};
+
 exports.help = {
   name: 'unban',
-  description: 'Herhangi bir kullanıcının IDsini belirterek yasapı kaldırabilirsiniz',
-  usage: 'unban <id>'
-}
+  description: 'banlıların yasaklarını kaldırır',
+  usage: 'unban'
+};
