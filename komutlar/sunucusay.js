7dcee1732c76c3f6633c18e5e597871154526803
Glitch (show-team-3mir) <none>
2021-01-25 20:05:30 +0000

    📙💰 Checkpoint ./komutlar/sunucusay.js:27438194/576 ./events/ready.js:27438194/130

---
+++ komutlar/sunucusay.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+
+exports.run = async (client, message, args) => {
+
+const naber = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setDescription(`Bot şu an ${client.guilds.size} sunucuya hizmet veriyor!`)
+        .setFooter(`DevTR`)
+        message.channel.send(naber);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ["drm", "sunucusay"],
+  permLevel: 0
+};
+exports.help = {
+  name: "sunucusay",
+  description: "Botun kaç sunucuda olduğunu gösterir.",
+  usage: "sunucusay"
+};
