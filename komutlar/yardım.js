de0c41fa1e887fe2577a153cea0752cd49ed0a77
Glitch (show-team-3mir) <none>
2021-01-25 19:32:20 +0000

    🚕👢 Checkpoint ./komutlar/yardım.js:27438194/1509

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -3,7 +3,7 @@ const Discord = require ("discord.js");
 exports.run = (client, message) => {
   if (!message.guild) {
     const ozelmesajuyari = new Discord.MessageEmbed()
-    .setColor()
+    .setColor(0xFF0000)
     .setTimestamp()
     .setAuthor(message.author.username, message.author.avatarURL)
     .addField('**Botumuz Maalesef Özel Mesajlarda Kullanılamaz**', `UYARI`)
@@ -43,22 +43,29 @@ return message.channel.send(EmbedCrewCode)
 .then;
 */
 const embed = new Discord.MessageEmbed  ()
-.setColor("Blue")
+.setColor("GOLD")
 .setTitle('Show Team Yardım ')
 .setImage('https://media.discordapp.net/attachments/803189389374652426/803191624435892244/standard.gif')
 .setDescription(`
 >    <a:9851_Loading:802635592928067584>  Show Team 1.4 Güncellemesi
 
 > <a:carkl:682484412499099652> [ **!moderasyon** ] → Moderasyon Komutları
+――――――――――――――――――――――――――――――――――
 > <a:devp:776699041933295627> [ **!moderasyon2** ] → Yetkili Komutları 2
+――――――――――――――――――――――――――――――――――
 > <a:kullanici:686200700820389909>  [ **!kullanıcı** ] → Kullanıcı Komutları
+――――――――――――――――――――――――――――――――――
 > <a:707688648966930444:789460996938792970>  [ !**gif-menü** ] Gif Komutları
+――――――――――――――――――――――――――――――――――
 > <a:onay:686199897367511040> [ **!koruma-yardım**] → Moderasyon Komutları
+――――――――――――――――――――――――――――――――――
 > <a:gold:686208112252026981> [ **!seviye-yardım**] → Seviye Komutları  
+――――――――――――――――――――――――――――――――――
 >     **<a:HaraketliEmoji198:802849819604680725> Bot Linkleri <a:HaraketliEmoji198:802849819604680725>**
 > <a:party:797015217963532289> Botu Davet Et [Tıkla!](https://discord.com/oauth2/authorize?client_id=794184837355274270&scope=bot&permissions=8)
-> <a:ayi:764799982154547222> Hatalar Var İse Destek Sunucusundan Bize Bildir Lütfen [Tıkla!](https://discord.gg/7KFJktCdYf)        
- 
+――――――――――――――――――――――――――――――――――
+> <a:ayi:764799982154547222> Hatalar Var İse Destek Sunucusundan Bize Bildir Lütfen [Tıkla!](https://discord.gg/RybaFGs2)        
+ ――――――――――――――――――――――――――――――――――
  
  
 `)
