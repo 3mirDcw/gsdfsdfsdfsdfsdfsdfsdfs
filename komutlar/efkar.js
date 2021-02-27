5ba22adccbef7c50b932ea619c54d40777a2abe2
Glitch (sturdy-rift-attic) <none>
2021-02-27 11:35:11 +0000

    🏗👵 Checkpoint ./komutlar/aduketçek.js:28703961/871 ./komutlar/ara155.js:28703961/1055 ./komutlar/balıktut.js:28703961/1004 ./komutlar/beşlik.js:28703961/819 ./komutlar/efkar.js:28703961/1351 ./komutlar/ejderha.js:28703961/625 ./komutlar/espri.js:28703961/1708 ./komutlar/hesapla.js:28703961/859 ./komutlar/kartopu:28703961/1102 ./komutlar/kartopu.js:28703961/27 ./komutlar/kaç-cm.js:28703961/1033 ./events/slots.js:28703961/931 ./komutlar/taksimdayı.js:28703961/713

--- komutlar/efkar.js
+++ komutlar/efkar.js
@@ -1,26 +1,22 @@
-const lucifeeer = require('discord.js');
+const Discord = require("discord.js");
 
-exports.run = async(client, message) => {
-    var Lucifer = message.mentions.users.first();
-    if(!Lucifer) return message.channel.send(new lucifeeer.MessageEmbed().setDescription("Lütfen Birisini Etiketleyin.").setColor("RED"))
+exports.run = (client, message) => {
 
-    var efkarLevel = ["%1","%2","%4","%5","%8","%9","%11","%15","%16","%19","%24","%25","%31"/*sj*/,"%42","%45","%46","%47","%49","%53","%54","%57","%58","%61","%68","%73","%75","%81","%84","%96","%100"]
+const FwhyCode = Math.floor(Math.random() * 100) + 1;
 
-    var efkarRandom = efkarLevel[Math.floor(Math.random() * efkarLevel.length)];
+return message.channel.send(`**FwhyCode** \n**Efkarınız:** **%${FwhyCode}** **Efkar** `);
 
-    message.channel.send(new lucifeeer.MessageEmbed().setDescription(`${efkarRandom} Efkarlısın. <a:B_Sigara:802635594358325269>`).setColor("BLACK"))
+};
 
-
-}
 exports.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases: ["efkar-ölç", "efkar"],
-    permLevel: 0
-  };
-  
-  exports.help = {
-    name: "efkar-ölç",
-    description: "dasdas",
-    usage: "!efkar"
-  };
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: "efkarım",
+  description: "FwhyCode | Efkarınızı ölçer",
+  usage: "efkar Ölçer"
+};
